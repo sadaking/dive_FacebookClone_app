6bc6bb8af1619af6a03b5f5730c4ac136885f30f
sadaking <sichirigahama.37@gmail.com>
2020-05-05 23:04:31 +0900

    PictureのCRUD機能作成準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'users#new'
+  get '/pictures', to: 'pictures#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
