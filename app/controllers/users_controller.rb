4ff2c0c2d19cb2003dca73321b1aab9d5ddc7860
sadaking <sichirigahama.37@gmail.com>
2020-05-05 22:58:19 +0900

    Usersコントローラの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect?to user_path(@user.id)
+      redirect_to user_path(@user.id)
     else
       render :new
     end
