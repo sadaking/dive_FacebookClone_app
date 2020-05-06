a2e1e6c5cde002318bc75448cf098c79c5d442a7
sadaking <sichirigahama.37@gmail.com>
2020-05-06 10:09:57 +0900

    ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'users#new'
-  get '/pictures', to: 'pictures#index'
+  resources :pictures
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
