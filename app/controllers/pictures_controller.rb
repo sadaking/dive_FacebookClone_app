b127aa70620e94e35dd0c8d60476f8f48ee41470
sadaking <sichirigahama.37@gmail.com>
2020-05-06 16:03:55 +0900

    投稿一覧の作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,61 @@
 class PicturesController < ApplicationController
+before_action :set_picture, only: [:show, :edit, :update, :destroy]
+
   def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = current_user.pictures.build(picture_params)
+    if params[:back]
+      render :new
+    else
+      if @picture.save
+        redirect_to pictures_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "投稿を編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture.destroy
+    redirect_to pictures_path, notice:"投稿を削除しました!"
+  end
+
+  def confirm
+    @picture = current_user.pictures.build(picture_params)
+    render :new if @picture.invalid?
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:title, :content)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
